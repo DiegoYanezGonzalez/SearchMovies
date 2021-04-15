80efcb29f45777966badf9b3b256b9359132f73a
diego yañez <diego.y8@gmail.com>
2021-04-15 17:00:09 -0400

    SearchFuncionalityRigth

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,13 @@
-import React,{Component} from 'react'
+import React,{Component} from 'react';
 
+
+
+const API_KEY= '4287ad07'
 
 export class SearchForm extends Component{
 
 state={
-  inputMovie:''
+  inputMovie:``
 }
 
 _handleChange=(e) =>{
@@ -14,11 +17,16 @@ _handleChange=(e) =>{
 
 _handleSubmit=(e) =>{
   e.preventDefault()
-  alert(this.state.inputMovie)
+  const {inputMovie} = this.state
+  fetch(` http://www.omdbapi.com/?apikey=${API_KEY}&s=${inputMovie}` )
+    .then(res =>res.json())
+    .then(results  => {
+      console.log(results)
+     const{Search,totalResults} = results
+     console.log({Search,totalResults})
+     this.props.onResults(Search)
+    })
 }
-
-
-
     render() {
         return(
       <form onSubmit={this._handleSubmit}>
